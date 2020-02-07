fafa0ac5f16c87441433f9e8a2aa4577be2f224d
Nathaly Otero <oteropaternina.n@gmail.com>
2020-02-07 10:49:23 -0500

    adding exercises

---
+++ Exercises/rangeSum/rangeSum.js
@@ -0,0 +1,30 @@
+// Given the root node of a binary search tree, return the sum of values of all 
+// nodes with value between L and R (inclusive).
+
+// The binary search tree is guaranteed to have unique values.
+
+// Example 1:
+
+// Input: root = [10,5,15,3,7,null,18], L = 7, R = 15
+// Output: 32
+
+// Example 2:
+
+// Input: root = [10,5,15,3,7,13,18,1,null,6], L = 6, R = 10
+// Output: 23
+ 
+
+// Note:
+
+// The number of nodes in the tree is at most 10000.
+// The final answer is guaranteed to be less than 2^31.
+
+function rangeSumBST(root,L,R){
+	const filtro = root.filter(elem => (elem >= L && elem <= R));
+	const sum = filtro.reduce((acc,cur) => acc + cur);
+	return sum;
+
+};
+
+console.log(rangeSumBST([10,5,15,3,7,null,18],7,15));
+console.log(rangeSumBST([10,5,15,3,7,13,18,1,null,6],6,10));
