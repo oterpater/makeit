1ee74c46e23988dba8571280a60d3f5e704f22fe
Nathaly Otero <oteropaternina.n@gmail.com>
2020-02-07 14:36:00 -0500

    adding exercise

---
+++ Exercises/balancedStrings/balancedStrings.js
@@ -0,0 +1,60 @@
+// Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+
+// Given a balanced string s split it in the maximum amount of balanced strings.
+
+//Dada una cadena s balanceada divide esta en la maxima cantidad de cadenas balanceadas
+
+// Return the maximum amount of splitted balanced strings.
+
+ 
+
+// Example 1:
+
+// Input: s = "RLRRLLRLRL"
+// Output: 4
+// Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring 
+// contains same number of 'L' and 'R'.
+// Example 2:
+
+// Input: s = "RLLLLRRRLR"
+// Output: 3
+// Explanation: s can be split into "RL", "LLLRRR", "LR", each substring 
+// contains same number of 'L' and 'R'.
+// Example 3:
+
+// Input: s = "LLLLRRRR"
+// Output: 1
+// Explanation: s can be split into "LLLLRRRR".
+// Example 4:
+
+// Input: s = "RLRRRLLRLL"
+// Output: 2
+// Explanation: s can be split into "RL", "RRRLLRLL", since each substring 
+// contains an equal number of 'L' and 'R'
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 1000
+// s[i] = 'L' or 'R'
+
+function balancedStringSplit(s){
+
+
+};
+
+console.log(balancedStringSplit("RLRRLLRLRL"));
+console.log(balancedStringSplit("LLLLRRRR"));
+
+
+
+
+
+
+
+
+
+
+
+
+
