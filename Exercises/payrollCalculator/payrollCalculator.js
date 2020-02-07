9c42992543b55471bd35bcedf93f39f88b66ac4e
Nathaly Otero <oteropaternina.n@gmail.com>
2020-02-07 09:02:45 -0500

    updating exercise

--- Exercises/payrollCalculator/payrollCalculator.js
+++ Exercises/payrollCalculator/payrollCalculator.js
@@ -50,6 +50,14 @@ class Managers{
     };
 };
 
+// class Managers extends Employees {
+//     constructor(name,baseSalary,bonus){
+//         super(name,baseSalary,salariesSum,healthFund,retirementFund);
+//         this.bonus = bonus;
+//     };
+// };
+
+
 const e1 = new Employees("Nathaly",31);
 const e2 = new Employees("Sara",31);
 const m1 = new Managers("Simon", 234, 3);
@@ -61,34 +69,33 @@ const m1 = new Managers("Simon", 234, 3);
 class Company{
     constructor(){
         this.employees = [];
-        this.managers = [];
+        // this.managers = [];
         this.paymentsSum = 0;
         this.healthFund = 0;
         this.retirementFund = 0;
     };
 
-    addEmployees(employee){              
-        if (Object.keys(employee).length === 5){
-           this.employees.push(employee);
-        } else {
-            this.managers.push(employee);
-        }
+    addEmployees(){    
+        for(let employee in arguments){
+            this.employees = this.employees.concat(arguments[employee]);        
+        };        
     };
 
     generatePayroll(){
-        const paymentsE = this.employees.map(elem => elem.baseSalary * 0.88);
-        const paymentE = paymentsE.reduce((acc,cur) => acc + cur);
+        const payments = this.employees.map(elem => elem.baseSalary * 0.88);
+        const payment = payments.reduce((acc,cur) => acc + cur);
         this.employees.map(elem => elem.salariesSum += elem.baseSalary * 0.88);
         this.employees.map(elem => elem.healthFund += elem.baseSalary * 0.04);
         this.employees.map(elem => elem.retirementFund += elem.baseSalary * 0.08);
 
-        const paymentsM = this.managers.map(elem => elem.baseSalary);
-        const paymentM = paymentsM.reduce((acc,cur) => acc + cur);
-        this.managers.map(elem => elem.salariesSum += elem.baseSalary * 0.88);
-        this.managers.map(elem => elem.healthFund += elem.baseSalary * 0.04);
-        this.managers.map(elem => elem.retirementFund += elem.baseSalary * 0.08);
+        // const paymentsM = this.managers.map(elem => elem.baseSalary + elem.bonus);
+        // const paymentM = paymentsM.reduce((acc,cur) => acc + cur);
+        // this.managers.map(elem => elem.salariesSum += elem.baseSalary * 0.88);
+        // this.managers.map(elem => elem.healthFund += elem.baseSalary * 0.04);
+        // this.managers.map(elem => elem.retirementFund += elem.baseSalary * 0.08);
 
-        const result = paymentE + paymentM;
+        const result = payment;
+        // paymentE + paymentM;
         this.paymentsSum += result;
         this.healthFund += 0.04*(result/0.88);
         this.retirementFund+= 0.08*(result/0.88);
@@ -97,30 +104,27 @@ class Company{
     };
 };
 
-// const company = new Company();
-// company.addEmployees(e1);
+const company = new Company();
+company.addEmployees(e1,e2,m1);
 // company.addEmployees(e2);
 // company.addEmployees(m1);
-// console.log("Pago 0");
-// console.log(company.employees);
-// console.log(company.managers);
-// console.log(company.paymentsSum);
-// console.log(company.healthFund);
-// console.log(company.retirementFund);
-// console.log("Pago 1");
-// console.log(company.generatePayroll());
-// console.log(company.employees);
-// console.log(company.managers);
-// console.log(company.paymentsSum);
-// console.log(company.healthFund);
-// console.log(company.retirementFund);
-// console.log("Pago 2");
-// console.log(company.generatePayroll());
-// console.log(company.employees);
-// console.log(company.managers);
-// console.log(company.paymentsSum);
-// console.log(company.healthFund);
-// console.log(company.retirementFund);
+console.log("Pago 0");
+console.log(company.employees);
+console.log(company.paymentsSum);
+console.log(company.healthFund);
+console.log(company.retirementFund);
+console.log("Pago 1");
+console.log(company.generatePayroll());
+console.log(company.employees);
+console.log(company.paymentsSum);
+console.log(company.healthFund);
+console.log(company.retirementFund);
+console.log("Pago 2");
+console.log(company.generatePayroll());
+console.log(company.employees);
+console.log(company.paymentsSum);
+console.log(company.healthFund);
+console.log(company.retirementFund);
 
 
 
