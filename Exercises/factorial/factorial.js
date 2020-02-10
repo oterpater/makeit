3954813e7f8125ecefa9b2ec3d9d9bf07ad64a01
Nathaly Otero <oteropaternina.n@gmail.com>
2020-02-10 14:47:05 -0500

    updating

--- Exercises/factorial/factorial.js
+++ Exercises/factorial/factorial.js
@@ -18,13 +18,15 @@ function factorial(num){
 
 //Recursivo
 function factorial(num){
-	if(num <= 1){
+	if (num === 0){
+		return 1;
+	} else if(num <= 1){
 		return num;
 	}
 	return num * factorial(num - 1);
 };
 
-console.log(factorial(4)); // 24
+console.log(factorial(0)); // 24
 console.log(factorial(5)); // 120
 console.log(factorial(6)); // 720
 console.log(factorial(9)); // 362880
