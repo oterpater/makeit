fe5c5ba12f497725fea4038e0d7c32c21ad33922
Nathaly Otero <oteropaternina.n@gmail.com>
2020-02-11 16:28:29 -0500

    adding node

---
+++ Exercises/node/index.js
@@ -0,0 +1,14 @@
+// Vamos crear una clase `Node` va a recibir como argumento 
+// un valor `value` y una referecia `ref`, por defecto la referencia 
+// es `undefined` y crea los atributos de la clase.
+
+class Node{
+	constructor(value, ref){
+		this.value = value;
+		this.ref = undefined;
+	};
+
+
+
+	
+};
